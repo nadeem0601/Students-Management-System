e7440be738d4dac17701b379f2594ed6c89d5b0d
rakshyak-98 <rakshak1998@gamil.com>
2024-12-02 23:12:52 +0530

    added endpoint for query students

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from motor.motor_asyncio import AsyncIOMotorClient
 from config.database import settings
 from bson import ObjectId
+from typing import Optional
 
 # Initialize FastAPI
 app = FastAPI()
@@ -14,7 +15,24 @@ async def root():
 
 @app.get("/students")
 async def fetch_all_students():
-    return await db["students"].find({}, {"_id": 0}).to_list();
+    return await db["students"].find({}, {"_id": 0}).to_list()
+
+@app.get("/students/search")
+async def search_student(name: Optional[str] = "", email: Optional[str] = "", course: Optional[str] = "", status: Optional[str] = "", student_class: Optional[str] = ""):
+    query = {}
+    if name:
+        query["name"] = {"$regex": name, "$options": "i"}
+    if email:
+        query["email"] = {"$regex": email, "$options": "i"}
+    if course:
+        query["courses"] = {"$regex": course, "$options": "i"}
+    if status:
+        query["status"] = {"$regex": status, "$options": "i"}
+    if student_class:
+        query["student_class"] = {"$regex": student_class, "$options": "i"}  # Case-insensitive search by class
+    print(query)
+    result = await db["students"].find(query, {"_id": 0}).to_list()
+    return result
 
 @app.get("/students/{id}")
 async def fetch_student_by_id(id: str):
@@ -29,6 +47,7 @@ async def delete_student(id: str):
 async def update_student(id: str, data: dict):
     result = await db["students"].update_one({"_id": ObjectId(id)}, {"$set": data})
     return result.modified_count > 0
+
 
 if __name__ == "__main__":
     import uvicorn
